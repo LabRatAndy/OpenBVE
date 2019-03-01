8019763efc66f15bc18b6a68e9852c9a7007cf1c
LabRatAndy <andrew.dawson@adbioscientist.co.uk>
2019-03-01 21:30:43 +0000

    Added properties to maesh arrays

--- source/ObjectViewer/MeshArrays.cs
+++ source/ObjectViewer/MeshArrays.cs
@@ -8,6 +8,22 @@ namespace OpenBve
         VertexArrayObject vao;
         VertexBufferObject vertices;
         ElementBufferObject ibo;
+        internal VertexArrayObject VAO
+        {
+            get { return vao; }
+            set { vao = value; }
+        }
+        internal VertexBufferObject VBO
+        {
+            get { return vertices; }
+            set { vertices = value; }
+        }
+        internal ElementBufferObject EBO
+        {
+            get { return ibo; }
+            set { ibo = value; }
+        }
+
     }
 
 }
