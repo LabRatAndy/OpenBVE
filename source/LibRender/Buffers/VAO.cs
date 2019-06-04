bf1759e247b251fa27b8c08f3b18afc32eee9afa
LabRatAndy <andrew.dawson@adbioscientist.co.uk>
2019-06-04 22:37:21 +0100

    Added comments to VAO class

--- source/LibRender/Buffers/VAO.cs
+++ source/LibRender/Buffers/VAO.cs
@@ -4,31 +4,58 @@ using OpenTK.Graphics.OpenGL;
 
 namespace LibRender
 {
+    /// <summary>
+    /// Class to repressenting an openGL VertexArrayObject
+    /// </summary>
     public class VertexArrayObject : IDisposable
     {
+        /// <summary>
+        /// openGL handle to the VAO object
+        /// </summary>
         private int handle;
+        /// <summary>
+        /// List of attributes attached to the VAO
+        /// </summary>
         private List<Attribute> attributeList = null;
+        /// <summary>
+        /// VBO attached to the VAO
+        /// </summary>
         private VertexBufferObject vbo = null;
+        /// <summary>
+        /// List of EBO attached to the VAO
+        /// </summary>
         private List<ElementBufferObject> iboList = null;
 
+        /// <summary>
+        /// Constructs the Vertex Array Object
+        /// </summary>
+        /// <param name="Attributes">List of attributes to attached the VAO object</param>
+        /// <param name="ibos">List of EBO attached to the VAO object</param>
+        /// <param name="VBO">VBO object attached to the VAO object</param>
         public VertexArrayObject(List<Attribute>Attributes, List<ElementBufferObject> ibos, VertexBufferObject VBO)
         {
             GL.GenVertexArrays(1, out handle);
             attributeList = Attributes;
             iboList = ibos;
             vbo = VBO;
-        }
-
-        public void Bind()
-        {
-            GL.BindVertexArray(handle);
             vbo.Bind();
-            foreach(ElementBufferObject ibo in iboList)
+            foreach (ElementBufferObject ibo in iboList)
             {
                 ibo.Bind();
             }
         }
 
+        /// <summary>
+        /// Binds / activates the VAO for use
+        /// </summary>
+        public void Bind()
+        {
+            GL.BindVertexArray(handle);
+        }
+
+        /// <summary>
+        /// Sets the attached attributes in openGL
+        /// </summary>
         public void SetAttributes()
         {
             foreach(Attribute attrib in attributeList)
@@ -38,6 +65,10 @@ namespace LibRender
             }
         }
         
+        /// <summary>
+        /// Draws the the attached EBOs
+        /// </summary>
+        /// <param name="primitiveType">The primitive type that forms the EBO</param>
         public void Draw(PrimitiveType primitiveType)
         {
             foreach(ElementBufferObject ibo in iboList)
@@ -46,11 +77,20 @@ namespace LibRender
             }
         }
 
+        /// <summary>
+        /// Draws the attached VBO
+        /// </summary>
+        /// <param name="first">The first vertex in the VBO to start  drawing from </param>
+        /// <param name="count">The number of vertices to draw</param>
+        /// <param name="primitiveType">The primitive type that forms the object in the VBO</param>
         public void Draw(int first, int count, PrimitiveType primitiveType)
         {
             vbo.Draw(primitiveType, first, count);
         }
 
+        /// <summary>
+        /// Cleans up the VAO handle and the attached VBO, EBO list and attribute list 
+        /// </summary>
         public void Dispose()
         {
             if (vbo != null) vbo.Dispose();
@@ -62,6 +102,9 @@ namespace LibRender
             GC.SuppressFinalize(this);
         }
 
+        /// <summary>
+        /// Finaliser
+        /// </summary>
         ~VertexArrayObject()
         {
             if (vbo != null) vbo.Dispose();
