03927eaf0900be967bdd0a92a440ec1d2f8438f5
LabRatAndy <andrew.dawson@adbioscientist.co.uk>
2019-07-11 22:05:30 +0100

    Added ShaderType Enums

---
+++ source/LibRender/Shader/ShaderTypeEnums.cs
@@ -0,0 +1,18 @@
+﻿
+namespace LibRender
+{
+    public enum ShaderType
+    {
+        DefaultShader = 0,
+        ColouredFaceShader = 1,
+        TexturedFaceShader = 2,
+        TexturedTransparentShader = 3,
+        TextShader = 4,
+        CustomShader1 = 5,
+        CustomShader2 = 6,
+        CustomShader3 = 7,
+        CustomShader4 = 8,
+        CustomShader5 = 9,
+    }
+
+}
