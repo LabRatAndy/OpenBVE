d8e61c8f61ca2282973335e511165d2f578d0806
LabRatAndy <andrew.dawson@adbioscientist.co.uk>
2019-07-19 21:35:49 +0100

    add initialising renderer and shader functions along with some membervariables

---
+++ source/LibRender/RendererShader.cs
@@ -0,0 +1,42 @@
+﻿using OpenTK;
+namespace LibRender
+{
+    /// <summary>
+    /// Renderer class members pretaining to the shader implementation
+    /// </summary>
+    public static partial class Renderer
+    {
+        /// <summary>
+        /// array containing the shaders as obtained by shaderenums
+        /// </summary>
+        internal static Shader[] shaderList;
+        /// <summary>
+        /// Records that the shader in that array has been loaded
+        /// </summary>
+        internal static bool[] ShadersLoaded;
+        /// <summary>
+        /// The VAO for drawing rendering the scene
+        /// </summary>
+        internal static VertexArrayObject VAO;
+        /// <summary>
+        /// The model matrix passed to the shader
+        /// </summary>
+        internal static Matrix4d modelTransform;
+        /// <summary>
+        /// The view matrix passed to the shader
+        /// </summary>
+        internal static Matrix4d ViewTransform;
+        /// <summary>
+        /// The prospective matrix passed to the shader
+        /// </summary>
+        internal static Matrix4d ProjectionTransform;
+        /// <summary>
+        /// The fied of view in radians for the scene. 
+        /// </summary>
+        internal static double fieldOfView;
+
+        internal static bool initialised = false;
+     
+    }
+
+}
---
+++ source/LibRender/RendererShaderFunctions.cs
@@ -0,0 +1,75 @@
+﻿using OpenBveApi.Textures;
+using OpenBveApi.Objects;
+using OpenBveApi.Hosts;
+using OpenTK.Graphics.OpenGL;
+using System.Collections.Generic;
+
+namespace LibRender
+{
+    public static partial class Renderer
+    {
+        static Renderer()
+        {
+            shaderList = new Shader[10];
+            ShadersLoaded = new bool[10];
+            for (int n = 0; n < 10; n++)
+            {
+                shaderList[n] = null;
+                ShadersLoaded[n] = false;
+            }
+        }
+
+        public static void LoadShader(ShaderTypeEnum shadertype, string vertexfilename, string fragmentfilename)
+        {
+            try
+            {
+                shaderList[(int)shadertype] = new Shader(vertexfilename, fragmentfilename);
+            }
+            catch(ShaderCompileException e)
+            {
+                currentHost.ReportProblem(ProblemType.UnexpectedException, e.Message);
+                currentHost.ReportProblem(ProblemType.PathNotFound, e.ShaderFileaname);
+                ShadersLoaded[(int)shadertype] = false;
+                return;
+            }
+            ShadersLoaded[(int)shadertype] = true;
+        }
+
+        public static void InitialiseRenderer()
+        {
+            // check shaders have been loaded 0 to 4 mandatory will throw exception and refuse to run
+            if (ShadersLoaded[0] && ShadersLoaded[1] && ShadersLoaded[2] && ShadersLoaded[3] && ShadersLoaded[4])
+            {
+                currentHost.ReportProblem(ProblemType.UnexpectedException, "Shaders not loaded cannot initalise renderer");
+                initialised = false;
+                return;
+            }
+            //setup the attributes for the VAOs
+            List<Attribute> attributeList = new List<Attribute>();
+            Attribute Coordinates = new Attribute("Coordinates", 3, 0, 8 * sizeof(double), 0, VertexAttribPointerType.Double);
+            Attribute Normal = new Attribute("Normal", 3, 3 * sizeof(double), 8 * sizeof(double), 1, VertexAttribPointerType.Double);
+            Attribute TexCoords = new Attribute("TexCoords", 2, 6 * sizeof(double), 8 * sizeof(double), 2, VertexAttribPointerType.Double);
+            attributeList.Add(Coordinates);
+            attributeList.Add(Normal);
+            attributeList.Add(TexCoords);
+            //create VAO
+            VAO = new VertexArrayObject(attributeList);
+            VAO.Bind();
+            VAO.SetAttributes();
+            VAO.UnBind();
+            initialised = true;
+        }
+
+        public static double FieldOfView
+        {
+            get { return fieldOfView; }
+            set { fieldOfView = value; }
+        }
+
+        public static void UpdateFrame()
+        {
+
+        }
+    }
+
+}
