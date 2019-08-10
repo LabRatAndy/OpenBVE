f8cf4ffc5b9784a66b2dba39fee5a5dab9c14950
LabRatAndy <andrew.dawson@adbioscientist.co.uk>
2019-08-10 21:48:07 +0100

    Added EBOFaceType enum to improve readablity of code in librender

---
+++ source/LibRender/Buffers/EBOTypes.cs
@@ -0,0 +1,16 @@
+﻿namespace LibRender
+{
+    /// <summary>
+    /// The face type of the EBO. ie which shader to use
+    /// </summary>
+    internal enum EBOFaceType
+    {
+        /// <summary>Plain old RGBA coloured face </summary>
+        ColouredFace = 1,
+        /// <summary>Textured face without a transparency </summary>
+        TexturedFace = 2,
+        /// <summary>Textured face with a transparancy </summary>
+        Transparent = 3,
+    }
+
+}
