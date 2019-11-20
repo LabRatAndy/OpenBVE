042c6ca4224f64726514dcc735953c0cfbcad142
LabRatAndy <andrew.dawson@adbioscientist.co.uk>
2019-11-20 13:12:39 +0000

    Begun creating my version of shader using renderer starting with shader class

---
+++ source/LibRender3/Shader/Shader.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using OpenTK;
+using OpenTk.Graphics;
+using OpenTk.Graphics.OpenGL;
+
+namespace LibRender3.Shader
+{
+
+
+    internal class Shader : IDisposable
+    {
+        private readonly int handle;
+        private int vertexShaderhandle;
+        private int fragmentShaderhandle;
+        private int geometryShaderHandle;
+        private int tesscontrolShaderHandle;
+        private int tessevalShaderHandle;
+
+        internal Shader(string vertexShaderFilename, string fragmentShaderFilename)
+        {
+			
+        }
+		internal Shader(string vertexShaderFilename,string fragmentShaderFilename, string geomShaderfilename, string tessctrlShaderfilename, string tessevalShaderFilename)
+        {
+
+        }
+
+		internal void Activate()
+        {
+
+        }
+		internal void Deactivate()
+        {
+
+        }
+		internal int GetUniformHandle(string uniformName)
+        {
+
+        }
+		internal int GetAttributeHandle(string attributeName)
+        {
+			
+        }
+
+		private void LoadShader(string Shaderfilename, ShaderType shaderType)
+        {
+
+        }
+    }
+}
