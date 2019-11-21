6522400f9e2ebc8f9fdc6c6286aa6119f6a09157
LabRatAndy <andrew.dawson@adbioscientist.co.uk>
2019-11-21 21:22:10 +0000

    Added Vertex Struct

---
+++ source/LibRender3/Buffers/RenderVertex.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.InteropServices;
+using OpenBveApi.Math;
+
+namespace LibRender3
+{
+	[StructLayout(LayoutKind.Sequential)]
+	internal struct RenderVertex
+	{
+		internal Vector3f Coordinates;
+		internal Vector3f Normals;
+		internal Vector2f TexCoordinates;
+
+		internal static int SizeInBytes => Marshal.SizeOf(typeof(RenderVertex));
+	}
+
+}
