af3ef940bc9b51ccf22ecb7be9a23200707bf87d
LabRatAndy <andrew.dawson@adbioscientist.co.uk>
2019-11-26 21:35:58 +0000

    Add uniform struct and uniformBufferobject

---
+++ source/LibRender3/Buffers/UniformBufferObject.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using OpenTK.Graphics.OpenGL;
+
+namespace LibRender3
+{
+	internal class UniformBufferObject : IDisposable
+	{
+		private Uniform[] uniforms;
+		private readonly int handle;
+		private readonly int lengthofarray;
+
+		internal UniformBufferObject(Uniform[] UBOs)
+		{
+			GL.GenBuffers(1, out handle);
+			uniforms = UBOs;
+			lengthofarray = UBOs.Length;
+		}
+		internal void SetBufferStrorage()
+		{
+			Bind();
+			GL.BufferStorage(BufferTarget.UniformBuffer, uniforms.Length * Uniform.SizeInBytes(), uniforms, BufferStorageFlags.MapWriteBit| 
+				BufferStorageFlags.MapCoherentBit| BufferStorageFlags.MapPersistentBit );
+			UnBind();
+		}
+		internal void Bind()
+		{
+			GL.BindBuffer(BufferTarget.UniformBuffer, handle);
+		}
+		internal void UnBind()
+		{
+			GL.BindBuffer(BufferTarget.UniformBuffer, 0);
+		}
+		internal Uniform[] Uniforms
+		{
+			get
+			{
+				return uniforms;
+			}
+			set
+			{
+				if(value.Length > lengthofarray) throw new OverflowException("Uniforms array cannot be set longer than the first uniform array");
+				uniforms = value;
+			}
+		}
+		public void Dispose()
+		{
+			GL.DeleteBuffer(handle);
+			GC.SuppressFinalize(this);
+		}
+		~UniformBufferObject()
+		{
+			GL.DeleteBuffer(handle);
+		}
+
+	}
+}
---
+++ source/LibRender3/Buffers/UniformStruct.cs
@@ -0,0 +1,21 @@
+﻿using System.Runtime.InteropServices;
+using OpenTK;
+
+namespace LibRender3
+{
+	[StructLayout(LayoutKind.Sequential)]
+	internal struct Uniform
+	{
+		internal Matrix4 modelTransform;
+		internal Matrix4 viewTransform;
+		internal Matrix4 projectionTransform;
+
+		internal Uniform(Matrix4d model, Matrix4d view, Matrix4d projection)
+		{
+		}
+		internal static int SizeInBytes()
+		{
+			return Marshal.SizeOf(typeof(Uniform));
+		}
+	}
+}
