da60a3a88fb227ad4c401e31dcb072ad6c55cfd3
LabRatAndy <andrew.dawson@adbioscientist.co.uk>
2019-11-29 19:14:27 +0000

    Added structure type enum

---
+++ source/OpenBveApi/Objects/ObjectTypes/ObjectStructureType.cs
@@ -0,0 +1,25 @@
+﻿namespace OpenBveApi.Objects
+{
+	public enum StructureType
+    {
+		Freeobj,
+		WallL,
+		WallR,
+		DykeL,
+		DykeR,
+		Pole,
+		Rail,
+		Ground,
+		Beacon,
+		FormL,
+		FormR,
+		FormCL,
+		FormCR,
+		RoofL,
+		RoofR,
+		RoofCL,
+		RoofCR,
+		CrackL,
+		CrackR
+    }
+}
