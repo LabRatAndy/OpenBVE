9ea6c8cf5430f2269382070e3be10db98709f37c
LabRatAndy <andrew.dawson@adbioscientist.co.uk>
2019-11-29 21:47:08 +0000

    Added objectinstance struct

---
+++ source/LibRender3/ObjectLists/ObjectInstance.cs
@@ -0,0 +1,23 @@
+﻿using OpenBveApi.Objects;
+using OpenBveApi.Math;
+
+namespace LibRender3
+{
+	internal struct ObjectInstance
+	{
+		internal StructureType type;
+		internal ushort uniqueObjectIndex;
+		internal ushort uniqueObjectInstanceID;
+		internal Matrix4D modelTransform;
+		internal bool visible;
+
+		internal ObjectInstance(StructureType type, ushort objectindex, ushort objectinstance, Matrix4D transform, bool visible)
+		{
+			this.type = type;
+			uniqueObjectIndex = objectindex;
+			uniqueObjectInstanceID = objectinstance;
+			modelTransform = transform;
+			this.visible = visible;
+		}
+	}
+}
